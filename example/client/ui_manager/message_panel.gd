2bee6b56011c9a5dbc635002281f5a93b39bc377
Carlos Daniel S. C. <97362905+carlosdscunha@users.noreply.github.com>
2025-12-21 11:19:20 -0400

    Create message_panel.gd

---
+++ example/client/ui_manager/message_panel.gd
@@ -0,0 +1,8 @@
+# No progeto do cliente class Message_panel
+extends PanelContainer
+
+@onready var _message_text_edit : TextEdit = $VBoxContainer/MessageTextEdit
+
+func _on_send_button_button_up():
+	#rpc("receive_chat_message", multiplayer.get_unique_id(), _message_text_edit.text)
+	NetworkManager.receive_chat_message.rpc_id(1, multiplayer.get_unique_id(),_message_text_edit.text)
