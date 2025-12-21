bbee91551992c0c411cc45591c0b5cf3f277b8df
Carlos Daniel S. C. <97362905+carlosdscunha@users.noreply.github.com>
2025-12-21 11:23:04 -0400

    Add example client

--- example/client/ui_manager/message_panel.gd
+++ example/client/ui_manager/message_panel.gd
@@ -6,3 +6,7 @@ extends PanelContainer
 func _on_send_button_button_up():
 	#rpc("receive_chat_message", multiplayer.get_unique_id(), _message_text_edit.text)
 	NetworkManager.receive_chat_message.rpc_id(1, multiplayer.get_unique_id(),_message_text_edit.text)
+
+
+
+
